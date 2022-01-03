441220922f6e4284d16105c0c3d6eb9068bda5ca
Gerd <b324017@mg200.hpc.dkrz.de>
2022-01-03 11:10:42 +0100

    cleanup

---
+++ Lenet-5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##SBATCH --exclusive            # Do not share node
+##SBATCH --time=00:01:00        # Set a limit on the total run time
+##SBATCH --mail-type=FAIL       # Notify user by email in case of job failure
+##SBATCH --nodes=1              # Specify number of nodes
+##SBATCH --constraint=m40       # Constraint for node selection
+#SBATCH --job-name=Lenet-5      # Specify job name
+#SBATCH --partition=gpu         # Specify partition name
+#SBATCH --mem=256G              # 
+#SBATCH --account=bb1152        # Charge resources on this project account
+#SBATCH --output=%x.cape.out         # File name for standard output
+#SBATCH --error=data/DE.24/28x28/%x.cape.log     # File name for standard error output
+
+module purge
+module load cuda/10.0.130
+
+srun caffe train -solver data/DE.24/28x28/Lenet-5.cape_solver.prototxt
+
+# srun caffe train -solver data/DE.24/28x28/Lenet-5.cape_solver.prototxt -snapshot data/DE.24/28x28/Lenet-5.cape_iter_5000.solverstate
