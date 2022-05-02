39d5c011826e1049d15cd0f74edd02cb64e3caf5
Gerd Bürger <gerd.buerger@uni-potsdam.de>
2022-05-02 20:40:30 +0200

    add read_ana

---
+++ fun/read_ana.m
@@ -0,0 +1,36 @@
+## usage: retval = read_ana (GLON, GLAT, NH, SVAR={})
+##
+## read ERA5 data
+function retval = read_ana (GLON, GLAT, NH, SVAR={})
+
+   pkg load netcdf
+   F = glob("data/ind/*.nc")' ;
+   lon = ncread(F{1}, "longitude") ;
+   lat = ncread(F{1}, "latitude") ;
+   if (Llat = lat(1) > lat(end)) lat = flip(lat) ; endif
+   JLON = GLON(1) <= lon & lon <= GLON(2) ;
+   JLAT = GLAT(1) <= lat & lat <= GLAT(2) ;
+
+   for j = 1 : length(F)
+      nc = ncinfo(F{j}) ;
+      k = find(cellfun(@(c) length(c), {nc.Variables.Size}) > 1) ;
+      VAR(j) = v = {nc.Variables.Name}(k) ;
+      if ~ismember(v, SVAR) continue ; endif
+      LVAR{j} = nc.Variables(k).Attributes(6).Value ;
+      x = squeeze(ncread(F{j}, v{:})) ;
+      x = permute(x, [3 1 2]) ; # all data are N x W x H
+      if Llat x = flip(x, 3) ; endif
+      id = nctime(F{j}) ;
+      [id x] = selmon(id, x) ;
+      eval(sprintf("retval.%s.nc = ncinfo(F{j}) ;", v{:})) ;
+      eval(sprintf("retval.%s.id = id ;", v{:})) ;
+      eval(sprintf("retval.%s.x = x(:,JLON,JLAT) ;", v{:})) ;
+      eval(sprintf("retval.%s.lon = lon(JLON) ;", v{:})) ;
+      eval(sprintf("retval.%s.lat = lat(JLAT) ;", v{:})) ;
+      eval(sprintf("retval.%s.name = VAR{j} ;", v{:})) ;
+      ## aggregate
+      eval(sprintf("retval.%s = agg(retval.%s, NH) ;", v{:}, v{:})) ;
+
+   endfor
+
+endfunction
