gfbuerger/CARLOFFF

commit 63d6595f84425478b755eb1d05cd10d7d08e6644

add convection threshold

carlofff.m
@@ -1,5 +1,5 @@
 
-global isoctave LON LAT GREG REG NH MON IMB
+global isoctave LON LAT GREG REG NH MON IMB CNVDUR
 
 set(0, "defaultaxesfontsize", 26, "defaulttextfontsize", 30) ;
 
@@ -18,6 +18,7 @@ LON = GLON ; LAT = GLAT ; REG = "DE" ; # whole Germany
 ##GLON = LON ; GLAT = LAT ; GREG = REG ;
 ID = [2001 5 1 0 ; 2020 8 31 23] ;
 MON = 5 : 8 ;
+CNVDUR = 3 ;
 NH = 24 ; # relevant hours
 scale = 0.00390625 ; % MNIST
 Q0 = 0.99 ;
@@ -140,7 +141,7 @@ else
    clear skl ;
    for jMDL = 1 : length(MDL)
       mdl = MDL{jMDL} ;
-      if isnewer(sfile = sprintf("data/%s.%02d/Shallow.%s.%s.%s.ot", REG, NH, mdl, ptr.ind, pdd.name), ptfile)
+      if isnewer(sfile = sprintf("data/%s.%02d/Shallow.%s.%s.%s.ot", REG, NH, mdl, ptr.ind, pdd.name), ptfile, pdfile)
 	 printf("<-- %s\n", sfile) ;
 	 load(sfile) ;
       else

fun/read_klamex.m
@@ -3,23 +3,21 @@
 %%
 function s = read_klamex (fin)
 
-   global isoctave LON LAT MON REG
+   global isoctave LON LAT MON REG CNVDUR
 
    if isoctave(), pkg load io ; end
 
    dlon = 62 ; dlat = 63 ;
-   Jx = 23 : 29 ;
+   Jx = [9 10 23 : 29] ;
    
-   MONS = 5:8 ;
-   HRS = 12:23 ;
-
    fid = fopen(fin, "rt") ;
    str = fgetl(fid) ;
    s.vars = strsplit(str, ",")(Jx) ;
    fclose(fid) ;
    
-   D = dlmread(fin) ;
-   D = D(2:end,:) ;
+   D = real(dlmread(fin)) ;
+   Icnv = (D(2:end,Jx(1)) <= CNVDUR) ;
+   D = D(Icnv,:) ;
 
    lon = D(:,dlon) ; lat = D(:,dlat) ;
    I = lookup(LON, lon) == 1 & lookup(LAT, lat) == 1 ;

fun/selpdd.m
@@ -38,6 +38,7 @@ function pdd = selpdd (PDD, LON, LAT, ID, Q0, s)
 	 plot_pdd(pdd, jVAR) ;
       endfor
    endif
+
    pdd.x(isinf(pdd.x)) = 0 ;
    if 0
       plot_hrs(pdd) ;

commit c75fbbbabc6da370a9c6b8a2a98959a0bbdbd468

use Eta, fix error introduced by 63d6595f

carlofff.m
@@ -122,7 +122,8 @@ if 0
    disp(ndcorr(ptr.x(:,1,:,:), pdd.x)) ;
 endif
 
-w = squeeze(pdd.x(:,1,:,:)) ; # Eta
+jVAR = 3 ; # Eta
+w = squeeze(pdd.x(:,jVAR,:,:)) ;
 pdd.q = quantile(w(:), Q0) ;
 pdd.c = any(any(w > pdd.q, 2), 3) ;
 printf("class rates: %.1f %%  %.1f %%\n", 100 * [sum(w(:) > 0) sum(w(:) == 0)] / numel(w)) ;

plots.m
@@ -16,7 +16,7 @@ col = maxdistcolor(ncol = length(JMDL) + length(NET), @(m) sRGB_to_OSAUCS (m, tr
 
 ### cases
 load(sprintf("data/ana.%s.ob", GREG)) ;
-clf ; jVAR = 1 ;
+clf ; jVAR = 3 ;
 ## June 2013
 D = [2013, 5, 15 ; 2013, 6, 15] ; d = [2013 5 31] ;
 ## July 2014