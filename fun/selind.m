dc2fa29e9a9d2ae60ddf8ee95835c1fadbc8223b
Gerd Bürger <gerd.buerger@uni-potsdam.de>
2022-10-07 21:35:19 +0200

    rm img from selind

--- fun/selind.m
+++ fun/selind.m
@@ -5,13 +5,14 @@ function res = selind (s, ind, IND)
 
    res = s ;
 
+   if isequal(ind, IND) return ; endif
+
    indn = find(arrayfun(@(c) c == "1", ind)) ;
    INDn = find(arrayfun(@(c) c == "1", IND)) ;
    
    J = arrayfun(@(i) find(i == INDn), indn) ;
 
    res.x = s.x(:,J,:,:) ;
-   res.img = s.img(:,J,:,:) ;
    res.vars = s.vars(J) ;
    res.ind = ind ;
    
