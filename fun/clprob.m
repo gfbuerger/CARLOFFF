e21bd1ad41e359afbf18d055dbbd90c9f24e9d15
Gerd Bürger <gerd.buerger@uni-potsdam.de>
2023-03-30 18:08:03 +0200

    probabilistic tree predictions in clprob

--- fun/clprob.m
+++ fun/clprob.m
@@ -1,24 +1,19 @@
-## usage: prob = clprob (par, x, u)
+## usage: res = clprob (par, x, u)
 ##
-##
-function prob = clprob (par, x, u)
+## convert m5ppredict to prediction
+function res = clprob (par, x, u)
 
    global PMODE = "prob"
-   
-   w = m5ppredict(par, x) ;
 
    switch PMODE
-      case "det"
-	 [~, J] = min(abs(w - u), [], 2) ;
-	 d = cell2mat(arrayfun(@(i) nthargout(2, @min, sumsq(w(i,:) - u, 2)), 1 : size(w, 2), "UniformOutput", false)) ;
-	 prob = u(J)' ;
       case "prob"
-	 d = exp(-(mean(w, 2) - u).^2) ;
-	 prob = d ./ sum(d, 2) ;
+	 w = m5ppredict_new(par, x) ;
+	 J = cell2mat(arrayfun(@(j) nthargout(2, @min, abs(w(:,j) - u), [], 2), 1 : size(w, 2), "UniformOutput", false)) ;
+	 res = cell2mat(arrayfun(@(j) mean(J == u(j), 2), 1 : length(u), "UniformOutput", false)) ;
       otherwise
-	 prob = mean(w, 2) ;
+	 w = m5ppredict(par, x)(:,end) ;
+	 [~, J] = min(abs(w - u), [], 2) ;
+	 res = u(J)' ;
    endswitch
-
-   prob = prob(:,2) ;
 
 endfunction
